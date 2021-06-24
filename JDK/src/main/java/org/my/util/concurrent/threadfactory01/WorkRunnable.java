13b0f78c5bc223bbcff2033bc8a6619c9f8d1e32
lijie <lijielorry@aliyun.com>
2021-06-24 18:09:13 +0800

    add threadFactory

---
+++ JDK/src/main/java/org/my/util/concurrent/threadfactory01/WorkRunnable.java
@@ -0,0 +1,10 @@
+package org.my.util.concurrent.threadfactory01;
+
+public class WorkRunnable implements Runnable
+{
+	@Override
+	public void run() {
+		 System.out.println("complete a task");
+	}
+ 
+}
